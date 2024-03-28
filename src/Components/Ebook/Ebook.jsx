9454b9500ac5b26adb0caecc58e4ed217746d8af
Tahsin411934 <tahsin.msil.0503@gmail.com>
2024-03-28 15:01:08 +0600

    cart functionality added

--- src/Components/Ebook/Ebook.jsx
+++ src/Components/Ebook/Ebook.jsx
@@ -8,19 +8,79 @@ const Ebook = () => {
     const ebooks = useLoaderData()
 
     const [cart, setCart] = useState([])
+    const [totalPrice, setTotalPrice] = useState([])
+    const [title, setTitle] = useState([])
 
-    const AddToCart = (bookId) => {
-        const find= cart.find(item=>item==bookId);
+    const AddToCart = (bookId, bookName, price) => {
+        const dam = parseInt(price)
+        const find = cart.find(item => item == bookId);
         console.log(!find);
-        !find? setCart([...cart, bookId]):toast("Already Added To Cart!")
-        
+        if (!find) {
+            setCart([...cart, bookId]);
+            setTotalPrice([...totalPrice, dam]);
+            setTitle([...title, bookName])
+        } else {
+            toast("This Book Already Added To Cart!")
+        }
+
+
     }
+    function sumTotalPrice(totalPrice) {
+        let sum = 0;
+        for (let i = 0; i < cart.length; i++) {
+            sum += totalPrice[i];
+        }
+        return sum;
+    }
+    const total = sumTotalPrice(totalPrice)
 
     return (
         <>
+
+
+
+
+            <dialog id="my_modal_1" className="modal ">
+                <div className="modal-box bg-slate-400">
+                    <div className="flex items-center gap-16 text-lg">
+                        <div>
+                            <p>Book Id</p>
+                            {
+                                cart.map(c => <h3 key={c} className="font-bold text-lg text-center">{c}</h3>)
+                            }
+                        </div>
+                        <div>
+                            <p> Name</p>
+                            {
+                                title.map(t => <h3 key={t} className="font-bold text-lg">{t}</h3>)
+                            }
+                        </div>
+                        <div>
+                            <p> Price</p>
+                            {
+                                totalPrice.map(tp => <h3 key={tp} className="font-bold text-lg text-center">{tp}</h3>)
+                            }
+                        </div>
+                    </div>
+                    <hr className=' mb-2 h-[1px] border-none bg-slate-200 mx-auto w-[80%]' />
+                    <div className="flex items-center gap-10 ml-[22%]">
+                        <h1>Total Price</h1>
+                        <h1>{total}</h1>
+                    </div>
+
+                    <p className="py-4">Press ESC key or click the button below to close</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
             <div className="flex ml-[48%]">
-                < PiShoppingCart size={42} />
-                <h1 className="mt-6 text-[#CC3333]">{cart.length}</h1>
+                <button className="btn border-none" onClick={() => document.getElementById('my_modal_1').showModal()}>< PiShoppingCart size={42} /> <h1 className="mt-7 -ml-3 text-[#CC3333]">{cart.length}</h1></button>
+
+
                 <ToastContainer />
             </div>
 
@@ -30,7 +90,7 @@ const Ebook = () => {
                 }
 
             </div>
-            
+
         </>
     );
 };
--- src/Components/Ebook/EbookSingleItem.jsx
+++ src/Components/Ebook/EbookSingleItem.jsx
@@ -1,8 +1,9 @@
 
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+
 const EbookSingleItem = ({ebook, AddToCart}) => {
-    const { bookId, bookName, author, image, tags, } = ebook;
+    const {bookId,  bookName, author, image, category,price } = ebook;
 
 
     return (
@@ -13,15 +14,15 @@ const EbookSingleItem = ({ebook, AddToCart}) => {
                 </figure>
                 <div className="-mt-4 ml-10">
                     <div className='flex  gap-10'>
-                        <p className='bg-[#F3F3F3] p-1 rounded-lg text-base font-medium text-[#23BE0A]'>{tags[0]}</p>
-                        <p className='bg-[#F3F3F3] p-1 rounded-lg text-base font-medium text-[#23BE0A]'>{tags[1]}</p>
+                        <p className='bg-[#F3F3F3] p-1 rounded-lg text-lg  text-[#CC3333] font-bold'>{price} টাকা</p>
+                        <p className='bg-[#F3F3F3] p-1 rounded-lg text-base font-medium text-[#23BE0A]'>{category}</p>
                     </div>
                     <h2 className="card-title -mt-2">{bookName}</h2>
                     <span className='  card-title p-1 rounded-lg text-base font-medium text-[#424242] -mt-4'>By: {author}</span>
                     <hr className=' mb-2 h-[1px] border-none bg-slate-200 mx-auto w-[80%]' />
                     <div className='card-title  p-1 mb-2 rounded-lg text-base font-medium text-[#424242]  flex gap-5 items-center '>
                     <Link className=' no-underline  bg-[#59C6D2] text-[#fff] p-2 rounded-lg' to='https://www.banglabookshelf.com/Story%20Books/product_images/Humayun%20Ahmed/Himu/Moyurakkhi-By-Humayun-Ahmed.pdf' >Read Pdf</Link>
-                    <Link className=' no-underline  bg-[#23BE0A] text-[#fff] p-2 rounded-lg' onClick={()=>AddToCart(bookId)} >Add Cart</Link>
+                    <Link className=' no-underline  bg-[#23BE0A] text-[#fff] p-2 rounded-lg' onClick={()=>AddToCart(bookId,bookName,price)} >Add Cart</Link>
                         
                     </div>
                 </div>
